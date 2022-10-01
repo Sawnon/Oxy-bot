5a6638c9283eb490cb7e8d68bd0adb62b599c2f6
Sainyb <102361537+Sainyb@users.noreply.github.com>
2022-10-02 00:29:40 +0300

    Console

--- console.py
+++ console.py
@@ -11,29 +11,27 @@ prefix = decouple.config("prefix")
 
 client = commands.Bot(command_prefix=(prefix), description="", intents=discord.Intents.all())
 
-
-
-
-
-#Events
 @client.event
-async def  on_ready():
-    await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game('Oxy'))
-    print("\n Ready !")
+async def on_ready():
+  await client.change_presence(status=discord.Status.do_not_disturb,activity=discord.Game('Oxy'))
+  print("\n Ready !")
+
+
 
 @client.event
 async def on_member_join(member):
-    print(f'\n {member} has joined the server!');
-
+  print(f'\n {member} has joined the server!')
+  
 @client.event
 async def on_member_remove(member):
-    print(f'\n {member} has left the server.');
+  print(f'\n {member} has left the server.')
+
+
 
 @client.event
-async def on_command_error (ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Missing required argument.')
-
+async def on_command_error(ctx, error):
+  if isinstance(error, commands.MissingRequiredArgument):
+    await ctx.send('Missing required argument.')
 
 
 
@@ -41,41 +39,61 @@ async def on_command_error (ctx, error):
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'**pong** {round(client.latency * 1000 )} ms ')
+  await ctx.send(f'**pong** {round(client.latency * 1000 )} ms ')
+
+
+
 
 @client.command()
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount + 1)
-    await ctx.send(f'Cleared {amount} mesages!')
-    print(f'Cleared {amount} mesages!')
-    time.sleep(4)
-    await ctx.channel.purge(limit=1)
+  await ctx.channel.purge(limit=amount + 1)
+  await ctx.send(f'Cleared {amount} mesages!')
+  print(f'Cleared {amount} mesages!')
+  time.sleep(4)
+  await ctx.channel.purge(limit=1)
+
+
 
 @client.command()
 @commands.has_permissions(kick_members=True)
-async def kick(ctx, member : discord.Member, *,reason=None):
-    await member.kick(reason=reason);
-    await ctx.send(f'Member : {member} has been kicked.')
-    print(f'Member : {member} has been kicked.')
-    time.sleep(5)
-    await ctx.channel.purge(limit=1)
+async def kick(ctx, member: discord.Member, *, reason=None):
+  await member.kick(reason=reason)
+  await ctx.send(f'Member : {member} has been kicked.')
+  print(f'Member : {member} has been kicked.')
+  time.sleep(5)
+  await ctx.channel.purge(limit=1)
+
+
 
 @client.command()
 @commands.has_permissions(ban_members=True)
-async def ban(ctx, member : discord.Member, *,reason=None):
-    await member.ban(reason=reason);
-    await ctx.send(f'Member : {member} has been banned.')
-    print(f' Member : {member} has been banned.')
-    time.sleep(5)
-    await ctx.channel.purge(limit=1)
+async def ban(ctx, member: discord.Member, *, reason=None):
+  await member.ban(reason=reason)
+  await ctx.send(f'Member : {member} has been banned.')
+  print(f' Member : {member} has been banned.')
+  time.sleep(5)
+  await ctx.channel.purge(limit=1)
+
+
 
 @client.command()
-@commands.has_permissions(administrator=True)
-async def exit(ctx,):
-    print('\n Aborting...')
-    await ctx.send('Aborting...')
-    time.sleep(5)
-    os._exit(0)
+async def say(ctx, *, arg1):
+  argument1 = (arg1)
+  await ctx.channel.purge(limit=1)
+  await ctx.send(argument1)
+  pass
+
+
+
+
+
+@client.command()
+#@commands.has_permissions(manage_members=True)
+async def warn(ctx, member, *,reason):
+  await ctx.channel.purge(limit=1)
+  await ctx.send('Member has been warned \n - ')
+  await ctx.send(f'{member} you have been warned for,\n **{reason}**.')
 
 client.run(token)
+
